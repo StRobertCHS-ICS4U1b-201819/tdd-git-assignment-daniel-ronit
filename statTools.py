c78b54e7feba9890cbc447f1c92be03f844eccb7
DMansoori <36383418+DMansoori@users.noreply.github.com>
2018-11-07 18:25:24 -0500

    Update statTools.py

--- statTools.py
+++ statTools.py
@@ -1,1 +1,9 @@
-
+def findMeanOfList():
+  list = []
+  number = int(input("Enter your the Length of your List))
+  print("Enter your numbers")
+  for i in range (number):
+     data = int(input())
+     list.append(data)
+  mean = float(sum(list))/number
+  
