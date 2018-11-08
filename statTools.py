d3ccbde6c1b68ceee023b5076c4fdfbb5d385036
DMansoori <36383418+DMansoori@users.noreply.github.com>
2018-11-07 20:30:47 -0500

    Update statTools.py

--- statTools.py
+++ statTools.py
@@ -1,8 +1,17 @@
 def findMeanOfList():
   list = []
-  number = int(input("Enter your the Length of your List))
+  number = int(input("Enter the length of your list"))
   print("Enter your numbers")
   for i in range (number):
      data = int(input())
      list.append(data)
   mean = float(sum(list))/number
+
+ def findModeOfList():
+   list = []
+   number = int(input("Enter the length of your list"))
+   print("Enter your numbers")
+   for i in range (number):
+     data = int(input())
+     list.append(data)
+   return max(set(list)), key=list.count)
