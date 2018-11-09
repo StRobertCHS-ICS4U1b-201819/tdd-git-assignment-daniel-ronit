3d416d2c5f064726f226e9221bdabe6ea8928f5e
robuntu <robuntu@stro.ycdsb.ca>
2018-11-09 10:51:26 -0500

    second test passed

--- statTools.py
+++ statTools.py
@@ -1,4 +1,10 @@
 
 def lowerQuart(listofnums):
-    return 1
 
+    if (len(listofnums) == 1):
+        return listofnums[0]
+
+    if (len(listofnums) == 2):
+        return (listofnums[1] + listofnums[0])/2
+
+    if
--- test_statTools.py
+++ test_statTools.py
@@ -4,4 +4,5 @@ from statTools import *
 def test_lowerQuart_basic1():
     assert(lowerQuart([1]) == 1)
 
-
+def test_lowerQuart_basic1():
+    assert(lowerQuart([1, 2]) == 1.5)
