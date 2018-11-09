5f7ac08673e19651b930745af4890c39bb50e8ca
robuntu <robuntu@stro.ycdsb.ca>
2018-11-09 11:06:38 -0500

    lowerQuart changed to median and third test passes

--- statTools.py
+++ statTools.py
@@ -1,10 +1,11 @@
 
-def lowerQuart(listofnums):
+def median(listofnums):
 
     if (len(listofnums) == 1):
         return listofnums[0]
 
     if (len(listofnums) == 2):
-        return (listofnums[1] + listofnums[0])/2
+        return (listofnums[1] + listofnums[0])//2
 
-    if
+    if (len(listofnums) == 3):
+        return listofnums[(0 + (len(listofnums)-1))//2]
--- test_statTools.py
+++ test_statTools.py
@@ -1,8 +1,11 @@
 
 from statTools import *
 
-def test_lowerQuart_basic1():
-    assert(lowerQuart([1]) == 1)
+def test_median_basic1():
+    assert(median([1]) == 1)
 
-def test_lowerQuart_basic1():
-    assert(lowerQuart([1, 2]) == 1.5)
+def test_median_basic1():
+    assert(median([1, 2]) == 1.5)
+
+def test_median_basic1():
+    assert(median([1, 2, 3]) == 2)
